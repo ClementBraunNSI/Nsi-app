fa3a3873efb87b1240e7a67fa7f133ba2633b942
OuraClem <clement.braun1@outlook.fr>
2026-01-19 13:16:02 +0100

    klj

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,9 @@ export default {
   plugins: {
     '@tailwindcss/postcss': {},
   },
+  typescript: {
+    // !! ATTENTION !!
+    // Permet de déployer même s'il y a des erreurs TypeScript.
+    ignoreBuildErrors: true,
+  }
 }
